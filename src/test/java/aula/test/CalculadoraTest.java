0d3ebbfcdf20afbb08c5a1ea03d64f5716598449
Vítor Lessa <vitor@LAPTOP-B5OJ9H9E.(none)>
2020-04-21 13:11:10 -0300

    arquivo test TestInfo,TestReporter

--- src/test/java/aula/test/CalculadoraTest.java
+++ src/test/java/aula/test/CalculadoraTest.java
@@ -9,12 +9,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
 
 class CalculadoraTest {
 	Calculadora calc;
+	TestInfo testInfo;
+	TestReporter testReporter;
 	
 	@BeforeEach
-	void init() {
+	void init(TestInfo testInfo, TestReporter testReporter) {
+		this.testInfo = testInfo;
+		this.testReporter = testReporter;
 		System.out.println("----INICIO METODO----");
 		calc = new Calculadora();
 	}
@@ -26,6 +32,7 @@ class CalculadoraTest {
 		@Test
 		@DisplayName("CT001.01 - Teste Soma Valores Positivos")
 		void testeSoma01() {
+			testReporter.publishEntry("Foi executado: " + testInfo.getDisplayName());
 			int valorEsperado = 80;
 			int valorAtual = calc.somar(30, 50);
 			
@@ -35,6 +42,7 @@ class CalculadoraTest {
 		@Test
 		@DisplayName("CT001.02 - Teste Soma Valores Negativos")
 		void testeSoma02() {
+			testReporter.publishEntry("Foi executado: " + testInfo.getDisplayName());
 			int valorEsperado = -27;
 			int valorAtual = calc.somar(-12, -15);
 			
@@ -49,6 +57,7 @@ class CalculadoraTest {
 		@Test
 		@DisplayName("CT002-01 - Teste Subtrair Positivo e Negativo")
 		void testeSubtrair01() {
+			testReporter.publishEntry("Foi executado: " + testInfo.getDisplayName());
 			int valorEsperado = 80;
 			int valorAtual = calc.subtrair(50, -30);
 			
@@ -58,6 +67,7 @@ class CalculadoraTest {
 		@Test
 		@DisplayName("CT002-02 - Teste Subtrair Zero e Positivo")
 		void testeSubtrair02() {
+			testReporter.publishEntry("Foi executado: " + testInfo.getDisplayName());
 			int valorEsperado = 30;
 			int valorAtual = calc.subtrair(0, -30);
 			
@@ -68,6 +78,7 @@ class CalculadoraTest {
 	@Test
 	@DisplayName("Cenario003 - Teste Multiplicacao Varios Valores")
 	void testeMultiplicaNValores() {
+		testReporter.publishEntry("Foi executado: " + testInfo.getDisplayName());
 		assertAll(
 				() -> assertEquals(-4, calc.mutiplicar(2, -2)),
 				() -> assertEquals(14, calc.mutiplicar(7, 2)),
@@ -83,6 +94,7 @@ class CalculadoraTest {
 		@Test
 		@DisplayName("CT004-01 - Teste Divisao Valores Diferente de Zero")
 		void testeDivisao01() {
+			testReporter.publishEntry("Foi executado: " + testInfo.getDisplayName());
 			double valorEsperado = 7;
 			double valorAtual = calc.dividir(14, 2);
 			
@@ -92,6 +104,7 @@ class CalculadoraTest {
 		@Test
 		@DisplayName("CT004-02 - Teste Divisao com Zero")
 		void testeDivisao02() {
+			testReporter.publishEntry("Foi executado: " + testInfo.getDisplayName());
 			assertThrows(ArithmeticException.class, () -> calc.dividir(5,0), "Impossivel Dividir por Zero");
 		}
 		
